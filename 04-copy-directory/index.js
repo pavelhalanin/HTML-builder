56acebc306c2c87d9cdd3ebf2991e37a6ee8e4cf
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-08-09 00:42:30 +0300

    feat: task 04 updated

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,46 +1,71 @@
 const fs = require('fs');
 const path = require('path');
 
-async function copyDirectory(copy_from, copy_to) {
-  await createDirectory(copy_to);
-  const ARRAY = await getFolderInner(copy_from);
-
-  for (const FILE_OR_FOLDER of ARRAY) {
-    const PATH_FROM = path.join(copy_from, FILE_OR_FOLDER.name);
-    const PATH_TO = path.join(copy_to, FILE_OR_FOLDER.name);
-
-    const IS_DIRECTORY = FILE_OR_FOLDER.isDirectory();
-
-    if (IS_DIRECTORY) {
-      await copyDirectory(PATH_FROM, PATH_TO);
-      continue;
-    }
-
-    await fs.promises.copyFile(PATH_FROM, PATH_TO);
+class Task04Helper {
+  static logScore() {
+    console.log(
+      [
+        `\n< < < < < < < <`,
+        `Task 04 Copy directory`,
+        'EN',
+        `- [x] 0 / 30 After running 'node 04-copy-directory', the files-copy folder exists and exactly mirrors the contents of files`,
+        `- [x] 0 / 20 Rerunning the script after files are added/modified inside 'files' updates 'files-copy' accordingly`,
+        `- [x] 0 / 20 Rerunning the script after files are removed from 'files' also removes them from 'files-copy'`,
+        `RU:`,
+        `- [x] 0 / 30 После запуска 'node 04-copy-directory' папка files-copy существует и точно отражает содержимое files`,
+        `- [x] 0 / 20 Повторный запуск скрипта после добавления/изменения файлов в 'files' обновляет 'files-copy' соответствующим образом`,
+        `- [x] 0 / 20 Повторный запуск скрипта после удаления файлов из 'files' также удаляет их из 'files-copy'`,
+        `> > > > > > > >\n`,
+      ].join('\n'),
+    );
   }
-}
 
-async function getFolderInner(folderPath) {
-  return await fs.promises.readdir(folderPath, { withFileTypes: true });
-}
+  static async copyDirectory(copy_from, copy_to) {
+    await this.createDirectory(copy_to);
+    const ARRAY = await this.getFolderInner(copy_from);
 
-async function createDirectory(folderPath) {
-  await fs.promises.mkdir(folderPath, { recursive: true });
-}
+    for (const FILE_OR_FOLDER of ARRAY) {
+      const PATH_FROM = path.join(copy_from, FILE_OR_FOLDER.name);
+      const PATH_TO = path.join(copy_to, FILE_OR_FOLDER.name);
 
-async function recursiveRemove(folderPath) {
-  await fs.promises.rm(folderPath, {
-    recursive: true,
-    force: true,
-  });
+      const IS_DIRECTORY = FILE_OR_FOLDER.isDirectory();
+
+      if (IS_DIRECTORY) {
+        await this.copyDirectory(PATH_FROM, PATH_TO);
+        continue;
+      }
+
+      await fs.promises.copyFile(PATH_FROM, PATH_TO);
+    }
+  }
+
+  static async getFolderInner(folderPath) {
+    return await fs.promises.readdir(folderPath, { withFileTypes: true });
+  }
+
+  static async createDirectory(folderPath) {
+    await fs.promises.mkdir(folderPath, { recursive: true });
+  }
+
+  static async recursiveRemove(folderPath) {
+    await fs.promises.rm(folderPath, {
+      recursive: true,
+      force: true,
+    });
+  }
 }
 
 async function main() {
   try {
+    Task04Helper.logScore();
+
     const PATH_FROM = path.join(__dirname, './files');
     const PATH_TO = path.join(__dirname, './files-copy');
-    await recursiveRemove(PATH_TO);
-    copyDirectory(PATH_FROM, PATH_TO);
+
+    console.log(`Copy "${PATH_FROM}" to "${PATH_TO}"`);
+
+    await Task04Helper.recursiveRemove(PATH_TO);
+    Task04Helper.copyDirectory(PATH_FROM, PATH_TO);
   } catch (exception) {
     console.error(exception);
   }
