42dcbb1b9168f346a2392c7d3ed6ee0f8c6c8395
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-08-09 22:37:59 +0300

    feat: task 01 updated

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,22 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 class Task01Helper {
-  static logScore() {
-    console.log(
-      [
-        `\n< < < < < < < <`,
-        `Task 01 Read File [40 / 40]`,
-        'EN',
-        `- [x] 20 / 20 Running 'node 01-read-file' from the repository root prints the contents of 01-read-file/text.txt to the console`,
-        `- [x] 20 / 20 File reading is implemented with ReadStream; no synchronous fs calls are used`,
-        `RU:`,
-        `- [x] 20 / 20 Запуск 'node 01-read-file' из корня репозитория выводит содержимое 01-read-file/text.txt в консоль`,
-        `- [x] 20 / 20 Чтение файла реализовано с помощью ReadStream; синхронные вызовы fs не используются`,
-        `> > > > > > > >\n`,
-      ].join('\n'),
-    );
-  }
-
   static getFileContentByPath(filePath) {
     return new Promise((resolve, reject) => {
       const CHUNKS = [];
@@ -40,8 +24,6 @@ class Task01Helper {
 
 async function main() {
   try {
-    Task01Helper.logScore();
-
     const FILE_PATH = path.join(__dirname, 'text.txt');
     const FILE_CONTENT = await Task01Helper.getFileContentByPath(FILE_PATH);
 
