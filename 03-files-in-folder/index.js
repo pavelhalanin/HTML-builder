24de4515947c7e3817e4ff8c51915b181592e6ea
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-08-09 22:57:11 +0300

    feat: task 03 updated

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,43 +1,44 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 const path = require('path');
 
 class Task03Helper {
-  static logScore() {
-    console.log(
-      [
-        `\n< < < < < < < <`,
-        `Task 03 Files in folder [50 / 50]`,
-        'EN',
-        `- [x] 15 / 15 Running 'node 03-files-in-folder' lists files from 03-files-in-folder/secret-folder to the console`,
-        `- [x] 20 / 20 Each line is formatted as '<file name> - <extension> - <size>'`,
-        `- [x] 15 / 15 Subdirectories are not listed; only files directly inside secret-folder appear`,
-        `RU:`,
-        `- [x] 15 / 15 Запуск 'node 03-files-in-folder' выводит список файлов из 03-files-in-folder/secret-folder в консоль`,
-        `- [x] 20 / 20 Каждая строка отформатирована как '<имя файла> - <расширение> - <размер>'`,
-        `- [x] 15 / 15 Подпапки не перечисляются; отображаются только файлы, находящиеся непосредственно внутри secret-folder`,
-        `> > > > > > > >\n`,
-      ].join('\n'),
-    );
+  static async printDirectoryInfoByPath(folderPath) {
+    const STATS = await fs.promises.stat(folderPath);
+
+    const IS_NOT_DIRECTORY = !STATS.isDirectory();
+    if (IS_NOT_DIRECTORY) {
+      console.log(`It is not a directory: ${folderPath}`);
+      return;
+    }
+
+    const DATA_ARRAY = await this.getFolderItemsDataByPath(folderPath);
+
+    if (DATA_ARRAY.length == 0) {
+      console.log(`Folder is empty: ${folderPath}`);
+      return;
+    }
+
+    console.log(`About files in folder '${folderPath}':`);
+    this.printFolderItemsDataAsHyphenatedStringByArray(DATA_ARRAY);
   }
 
   static async getFolderItemsDataByPath(FOLDER_PATH) {
-    const files = await fs.readdir(FOLDER_PATH, { withFileTypes: true });
+    const files = await fs.promises.readdir(FOLDER_PATH, {
+      withFileTypes: true,
+    });
 
     const DATA_ARRAY = [];
     for (const file of files) {
       if (file.isFile()) {
         const FILE_PATH = path.join(FOLDER_PATH, file.name);
-        const STATS = await fs.stat(FILE_PATH);
+        const STATS = await fs.promises.stat(FILE_PATH);
         const BYTES = STATS.size;
         const KILO_BYTES = BYTES / 1024;
 
         DATA_ARRAY.push({
           ext: path.extname(file.name).slice(1),
-          kbNoFormatted: KILO_BYTES,
-          kb: Number(KILO_BYTES).toFixed(10),
+          kbNoRounded: KILO_BYTES,
           name: path.parse(file.name).name,
-          lastOpenAt: new Date(STATS.atimeMs).toLocaleString(),
-          createAt: new Date(STATS.birthtimeMs).toLocaleString(),
         });
       }
     }
@@ -45,103 +46,25 @@ class Task03Helper {
     return DATA_ARRAY;
   }
 
-  static printFolderItemsDataAsBeautifulTableByArray(DATA_ARRAY) {
-    const TITLE = {
-      name: 'File',
-      ext: 'Extension',
-      kb: 'Size (KB)',
-      lastOpenAt: 'Last open',
-      createAt: 'Created',
-    };
-
-    const KEYS = Object.keys(TITLE);
-
-    const SIZE = {};
-    KEYS.forEach((key) => {
-      SIZE[key] = TITLE[key].length;
-    });
-
-    for (let i = 0; i < DATA_ARRAY.length; i++) {
-      const CURRENT = DATA_ARRAY[i];
-
-      KEYS.forEach((key) => {
-        const length = String(CURRENT[key]).length;
-        if (length > SIZE[key]) {
-          SIZE[key] = length;
-        }
-      });
-    }
-
-    if (DATA_ARRAY.length == 0) {
-      return;
-    }
-
-    console.log(`< < < < < < < < (Beautiful table)\n`);
-
-    console.log(
-      KEYS.map((key) => {
-        return `${TITLE[key]}`.padStart(SIZE[key], ' ');
-      }).join(' | '),
-    );
-
-    console.log(
-      KEYS.map((key) => {
-        return `-`.repeat(SIZE[key]);
-      }).join(' | '),
-    );
-
-    for (let i = 0; i < DATA_ARRAY.length; i++) {
-      const CURRENT = DATA_ARRAY[i];
-
-      console.log(
-        KEYS.map((key) => {
-          return `${CURRENT[key]}`.padStart(SIZE[key], ' ');
-        }).join(' | '),
-      );
-    }
-
-    console.log('\n> > > > > > > > (End Beautiful table)');
-  }
-
   static printFolderItemsDataAsHyphenatedStringByArray(DATA_ARRAY) {
-    console.log(
-      '< < < < < < < < [ {fileName} - {fileExtension} - {fileSize}kb ]\n',
-    );
-
     for (let i = 0; i < DATA_ARRAY.length; i++) {
       const CURRENT = DATA_ARRAY[i];
-
-      console.log(
-        [CURRENT.name, CURRENT.ext, `${CURRENT.kbNoFormatted}kb`].join(' - '),
-      );
+      const VALUE = [
+        CURRENT.name,
+        CURRENT.ext,
+        `${CURRENT.kbNoRounded}kb`,
+      ].join(' - ');
+      console.log(VALUE);
     }
-
-    console.log(
-      '\n> > > > > > > > [ {fileName} - {fileExtension} - {fileSize}kb ]',
-    );
   }
 }
 
 async function main() {
   try {
-    Task03Helper.logScore();
-
     const FOLDER_PATH = path.join(__dirname, 'secret-folder');
-    const DATA_ARRAY = await Task03Helper.getFolderItemsDataByPath(FOLDER_PATH);
-
-    if (DATA_ARRAY.length == 0) {
-      console.log(`Folder is empty: ${FOLDER_PATH}\n`);
-      return;
-    }
-
-    console.log(`About files in folder '${FOLDER_PATH}':\n`);
-
-    Task03Helper.printFolderItemsDataAsBeautifulTableByArray(DATA_ARRAY);
-    console.log('\n');
-    Task03Helper.printFolderItemsDataAsHyphenatedStringByArray(DATA_ARRAY);
-  } catch (error) {
-    console.error(error);
-    console.error(error.message);
+    Task03Helper.printDirectoryInfoByPath(FOLDER_PATH);
+  } catch (exception) {
+    console.error(exception);
   }
 }
 
